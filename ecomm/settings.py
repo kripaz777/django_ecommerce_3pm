c9b901056f0d27aa619b084f8ff994205755d8b7
Aayush <aceraayush@gmail.com>
2023-02-03 16:04:22 +0545

    front page

--- ecomm/settings.py
+++ ecomm/settings.py
@@ -112,6 +112,12 @@ USE_TZ = True
 
 STATIC_URL = "static/"
 STATICFILES_DIRS = [BASE_DIR / "static",]
+import os
+# Base url to serve media files
+MEDIA_URL = '/media/'
+
+# Path where media is stored
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media/')
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
