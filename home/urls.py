4402f6b56837578597ce93a93e6db91d01203f73
Aayush <aceraayush@gmail.com>
2023-02-07 15:59:41 +0545

    cat abd brand

--- home/urls.py
+++ home/urls.py
@@ -3,4 +3,6 @@ from django.urls import path
 
 urlpatterns = [
     path('', HomeView.as_view(), name='home'),
+    path('category/<slug>', CategoryView.as_view(), name='category'),
+    path('brand/<slug>', BrandView.as_view(), name='brand'),
 ]
--- home/views.py
+++ home/views.py
@@ -19,3 +19,16 @@ class HomeView(BaseView):
         self.views['hot_products'] = Product.objects.filter(labels='hot')
         self.views['sale_products'] = Product.objects.filter(labels='sale')
         return render(request,'index.html',self.views)
+
+
+class CategoryView(BaseView):
+    def get(self,request,slug):
+        ids = Category.objects.get(slug = slug).id
+        self.views['cat_products'] = Product.objects.filter(category_id = ids )
+        return render(request,'category.html',self.views)
+
+class BrandView(BaseView):
+    def get(self,request,slug):
+        ids = Brand.objects.get(slug = slug).id
+        self.views['brand_products'] = Product.objects.filter(brand_id = ids )
+        return render(request,'brand.html',self.views)
